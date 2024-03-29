327e5738e96c780cd7ec83a3776207fe7795a7c5
Dony8511 <81507996+Dony8511@users.noreply.github.com>
2024-03-29 20:34:56 +0300

    offer-block, circle menu fix

--- paporotnik/src/pages/Main.js
+++ paporotnik/src/pages/Main.js
@@ -4,21 +4,32 @@ import pic1 from './../img/unsplash_pYyOZ8q7AII.png'
 import pic2 from './../img/unsplash_28c3pajQlyI.png'
 import pic3 from './../img/unsplash_zacucUA5LhE.png'
 
+import Button from "../components/button/button";
+
 class Main extends Component {
     render() {
         return (
             <>
-                <div className='container'>
-                    <div className="content">
+                <div className='container offer-cont'>
+                    <div className="FirstBlock">
                         <div className="album">
                             <img src={pic1} alt="" className="pic"/>
                             <img src={pic2} alt="" className="pic"/>
                             <img src={pic3} alt="" className="pic"/>
                         </div>
                         <div className="offer">
-
+                            <h2 className="offer-title">О НАС</h2>
+                            <p className="offer-text">
+                                Ландшафтное агентство PAPOROTNIK за 15 лет работы стало ведущим агентством на рынке
+                                ландшафтного дизайна и в данный момент работает с самыми требовательными заказчиками.
+                            </p>
+                            <Button variant="dark" text="Подробнее" link="#"/>
                         </div>
                     </div>
+                </div>
+                <div className="container darkSection">
+                    asasfdasfd
+                    {/*<img src= alt=""/>*/}
                 </div>
             </>
         );
