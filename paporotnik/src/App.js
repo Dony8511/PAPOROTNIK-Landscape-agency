56b2a1de37c19b3c7c8895ece99b8d53b0ef4096
Ruslan228007 <132265464+Ruslan228007@users.noreply.github.com>
2024-03-29 13:51:28 +0300

    button

--- paporotnik/src/App.js
+++ paporotnik/src/App.js
@@ -3,14 +3,14 @@ import './css/index.scss';
 import Button from "./components/button/button";
 function App() {
   return (
-    <div className="App">
-      <div className="container">
+      <div className="App">
+        <div className="container">
           <img src={logo} className="logo" alt="logo" />
           <Button variant="dark" text="Button" link="#" />
-      {/*    ВАРИАНТЫ КНОПОК: "dark","dark-outline","white","white-outline"*/}
-      {/*    ЕСЛИ БУДЕТ КАКАЯТО ОШИБКА ИЗ ЗА СЦСС ТО НАПИШИ 'npm i sass' */}
+          {/*    ВАРИАНТЫ КНОПОК: "dark","dark-outline","white","white-outline"*/}
+          {/*    ЕСЛИ БУДЕТ КАКАЯТО ОШИБКА ИЗ ЗА СЦСС ТО НАПИШИ 'npm i sass' */}
+        </div>
       </div>
-    </div>
   );
 }
 
