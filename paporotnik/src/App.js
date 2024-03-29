a5e7835f50fab4df25dcd458270c379fbb6cbbed
Dony8511 <81507996+Dony8511@users.noreply.github.com>
2024-03-29 14:25:46 +0300

    init react-router

--- paporotnik/src/App.js
+++ paporotnik/src/App.js
@@ -1,17 +1,23 @@
 import logo from './img/Logo.png';
 import './css/index.scss';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Button from "./components/button/button";
+
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-          <img src={logo} className="logo" alt="logo" />
-          <Button variant="dark" text="Button" link="#" />
-      {/*    ВАРИАНТЫ КНОПОК: "dark","dark-outline","white","white-outline"*/}
-      {/*    ЕСЛИ БУДЕТ КАКАЯТО ОШИБКА ИЗ ЗА СЦСС ТО НАПИШИ 'npm i sass' */}
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <img src={logo} className="logo" alt="logo"/>
+                {/*<Button variant="dark" text="Button" link="#"/>*/}
+                {/*    ВАРИАНТЫ КНОПОК: "dark","dark-outline","white","white-outline"*/}
+                {/*    ЕСЛИ БУДЕТ КАКАЯТО ОШИБКА ИЗ ЗА СЦСС ТО НАПИШИ 'npm i sass' */}
+            </div>
+            <Routes>
+                <Route path='/' element={<Button variant="dark" text="Button" link="#"/>}/>
+                <Route path='/portfolio' element={<Button variant="dark-outline" text="Button" link="#"/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
